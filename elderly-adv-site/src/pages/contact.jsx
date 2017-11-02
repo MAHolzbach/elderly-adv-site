92f9f8a6b9a6606d79d47729e47225dd3d57c323
Mike Holzbach <maholzbach@gmail.com>
2017-11-02 17:21:19 -0400

    Started contact page...
    ...bit of styling.

---
+++ elderly-adv-site/src/pages/contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import styles from "./contact.module.css";
+
+export default () => (
+  <div className={styles.contactLanding}>
+    <Navbar />
+    <h1 className={styles.title}>Reach Out</h1>
+    <div className={styles.formDiv}>
+      <form className={styles.contactForm}>
+        <input placeholder="Name..." type="text" />
+        <input placeholder="Email..." type="email" />
+      </form>
+      <form className={styles.messageArea}>
+        <textarea rows="2" placeholder="How can we help?" />
+      </form>
+    </div>
+  </div>
+);
