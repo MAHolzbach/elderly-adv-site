24264160baadeeacd76ae842f3c212663a44d286
Mike Holzbach <maholzbach@gmail.com>
2017-11-08 16:58:15 -0500

    Bit of content...
    ...bit of styling. Mostly research and doc reading.

--- elderly-adv-site/src/pages/about.jsx
+++ elderly-adv-site/src/pages/about.jsx
@@ -6,19 +6,19 @@ import ServiceCard from "../components/ServiceCard";
 
 const employeeArray = [
   {
-    name: "Person 1",
+    name: "John Jazz",
     pic: employee1,
     description:
       " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi aliquid perspiciatis vitae sint, pariatur maxime, eum, iusto quasi doloremque facere explicabo provident optio magni veniam!"
   },
   {
-    name: "Person 1",
+    name: "Sue Smiles",
     pic: employee1,
     description:
       " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi aliquid perspiciatis vitae sint, pariatur maxime, eum, iusto quasi doloremque facere explicabo provident optio magni veniam!"
   },
   {
-    name: "Person 1",
+    name: "Phil Friendly",
     pic: employee1,
     description:
       " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi aliquid perspiciatis vitae sint, pariatur maxime, eum, iusto quasi doloremque facere explicabo provident optio magni veniam!"
@@ -26,17 +26,35 @@ const employeeArray = [
 ];
 
 export default () => (
-  <div className={styles.aboutLanding}>
-    <Navbar activeLink={"about"} />
-    <h1 className={styles.title}>Who We Are</h1>
-    <div className={styles.cardDisplay}>
-      {employeeArray.map(employee => (
-        <ServiceCard
-          serviceName={employee.name}
-          pic={employee.pic}
-          description={employee.description}
-        />
-      ))}
+  <div className={styles.aboutPage}>
+    <div className={styles.aboutLanding}>
+      <Navbar activeLink={"about"} />
+      <h1 className={styles.title}>Who We Are</h1>
+      <div className={styles.cardDisplay}>
+        {employeeArray.map(employee => (
+          <ServiceCard
+            serviceName={employee.name}
+            pic={employee.pic}
+            description={employee.description}
+          />
+        ))}
+      </div>
+    </div>
+    <div className={styles.storyBox}>
+      <p className={styles.storyField}>
+        <h1 className={styles.title}>Our Story</h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium
+        asperiores nihil, dignissimos dicta voluptatibus suscipit aperiam at
+        libero. Suscipit dolorem assumenda atque in non cumque provident
+        voluptate harum eos hic saepe, beatae, ratione nihil itaque dolor ipsa,
+        eligendi velit! Assumenda ullam sunt tempore saepe, similique enim ipsum
+        vero neque ea recusandae ipsa sequi. Exercitationem ratione, quasi velit
+        placeat error unde voluptatum aspernatur suscipit praesentium odit
+        molestias accusantium consequuntur similique nulla fuga nihil eveniet
+        reprehenderit autem fugiat quos. Non atque reprehenderit molestiae eum
+        culpa obcaecati eligendi totam quo officiis adipisci qui vel veritatis
+        illo, quam debitis omnis eveniet dolor nulla cupiditate.
+      </p>
     </div>
   </div>
 );
